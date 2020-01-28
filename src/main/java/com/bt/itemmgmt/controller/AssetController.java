358867a70afb617eb247a1fbf21d2b06ff98c014
prashdeep <pradeep.kumar44@gmail.com>
2020-01-28 12:15:35 +0530

    initial commit

---
+++ src/main/java/com/bt/itemmgmt/controller/AssetController.java
@@ -0,0 +1,15 @@
+package com.bt.itemmgmt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/assets/")
+public class AssetController {
+
+    @GetMapping
+    public String greet(){
+        return "Hello world";
+    }
+}
