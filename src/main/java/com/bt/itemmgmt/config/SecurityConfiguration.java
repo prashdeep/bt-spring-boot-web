e7ae7091d914d1115a1cd4f437469d453fb2d158
prashdeep <pradeep.kumar44@gmail.com>
2020-01-29 15:28:37 +0530

    committing the security configuration

--- src/main/java/com/bt/itemmgmt/config/SecurityConfiguration.java
+++ src/main/java/com/bt/itemmgmt/config/SecurityConfiguration.java
@@ -1,17 +1,23 @@
 package com.bt.itemmgmt.config;
 
+import com.bt.itemmgmt.service.DomainUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DomainUserDetailsService domainUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -35,7 +41,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-        authenticationManagerBuilder
+        /*authenticationManagerBuilder
                 .inMemoryAuthentication()
                 .passwordEncoder(passwordEncoder())
                 .withUser("kiran")
@@ -44,9 +50,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("vinay")
                 .password(passwordEncoder().encode("admin"))
-                .roles("USER", "ADMIN");
+                .roles("USER", "ADMIN");*/
 
-
+        authenticationManagerBuilder
+                .userDetailsService(this.domainUserDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
